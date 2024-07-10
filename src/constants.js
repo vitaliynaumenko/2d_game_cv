72c7cc21203dab16d317b4c269d15299dd41654c
vitaliynaumenko <bambiino89@gmail.com>
2024-07-10 14:21:38 +0300

    init commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const scaleFactor = 3;
+export const dialogueData = {
+  pc: 'This is my PC. I work mostly in JavaScript, TypeScript, React.js, HTML, Css/Pre-processors, GQL, Tailwind.css, Bootstrap',
+  exit: `If you want to exit my portfolio, just close the tab.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  library: `There are a lot of programming books on my shelves.
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube and TV.`,
+  resume: `This is my desk and on it is my resume. <a href="../public/Naumenko_CV_1.pdf" target="_blank">Download my CV</a>`
+}
---
+++ src/kaboomCtx.js
@@ -0,0 +1,11 @@
+import kaboom from "kaboom"
+
+export const k = kaboom({
+    global:true,
+    touchToMouse: true,
+    // keyboard:true,
+    // fullscreen:true,
+    scale:2,
+    clearColor:[0,0,0,1],
+    canvas: document.getElementById('game')
+})
