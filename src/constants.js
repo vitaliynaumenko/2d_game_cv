1a9f16066bd18904d3cd89098e39153ceeaa5c02
vitaliynaumenko <bambiino89@gmail.com>
2024-12-18 15:34:46 +0200

    init commit

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const scaleFactor = 3;
 export const dialogueData = {
-  pc: 'This is my PC. I work mostly in JavaScript, TypeScript, React.js, HTML, Css/Pre-processors, GQL, Tailwind.css, Bootstrap',
+  pc: 'This is my PC. I work mostly in JavaScript, TypeScript, React.js, Next.js, HTML, CSS, SCSS, GQL, Tailwind.css, Bootstrap',
   exit: `If you want to exit my portfolio, just close the tab.`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
    <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
@@ -9,5 +9,5 @@ export const dialogueData = {
   library: `There are a lot of programming books on my shelves.
   I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube and TV.`,
-  resume: `This is my desk and on it is my resume. <a href="../public/Naumenko_CV_1.pdf" target="_blank">Download my CV</a>`
+  resume: `This is my desk and on it is my resume. <a href="../public/vitalii_naumenko_cv_1.pdf" target="_blank">Download my CV</a>`
 }
--- src/kaboomCtx.js
+++ src/kaboomCtx.js
@@ -3,8 +3,8 @@ import kaboom from "kaboom"
 export const k = kaboom({
     global:true,
     touchToMouse: true,
-    // keyboard:true,
-    // fullscreen:true,
+    keyboard:true,
+    fullscreen:true,
     scale:2,
     clearColor:[0,0,0,1],
     canvas: document.getElementById('game')
